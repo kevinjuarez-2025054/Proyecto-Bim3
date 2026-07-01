553a228d58bf3a9d28d7de60135a38f8cdfc7f26
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-07-01 10:56:57 -0600

    feat: se creo interfas de la entidad DetallePedido

---
+++ RestNova/src/models/detallePedido.ts
@@ -0,0 +1,8 @@
+export interface DetallePedido {
+    id_detalle_pedido: number;
+    id_platillo: number;
+    cantidad: number;
+    subtotal: number;
+    numero_pedido: number;
+    id_empleado: number;
+}
