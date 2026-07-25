7a94f7ac9c9b6c3b66b7bc4c2588dadb881ce4cf
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-07-25 16:54:48 -0600

    feat: se subio el Repository de Platillo

---
+++ RestNova/src/data/platilloRepository.ts
@@ -0,0 +1,40 @@
+import connection from "../config/db";
+import { Platillo } from "../models/platillo";
+import { RowDataPacket, ResultSetHeader } from "mysql2";
+
+export class PlatilloRepository {
+    async obtenerPlatillos(): Promise<Platillo[]> {
+        const [rows] = await connection.query<RowDataPacket[]>("SELECT * FROM platillo");
+        return rows as Platillo[];
+    }
+
+    async obtenerPlatilloPorId(id: number): Promise<Platillo | undefined> {
+        const [rows] = await connection.query<RowDataPacket[]>(
+            "SELECT * FROM platillo WHERE id_platillo = ?", [id]
+        );
+        return rows[0] as Platillo | undefined;
+    }
+
+    async crearPlatillo(platillo: Platillo): Promise<Platillo> {
+        await connection.query<ResultSetHeader>(
+            "INSERT INTO platillo (nombre_platillo, descripcion, precio,id_categoria) VALUES (?,?,?,?)",
+            [platillo.nombre_platillo, platillo.descripcion_platillo, platillo.precio_platillo, platillo.id_categoria]
+        );
+        return platillo;
+    }
+
+    async actualizarPlatillo(id: number, platillo: Platillo): Promise<Platillo | undefined> {
+        const [result] = await connection.query<ResultSetHeader>(
+            "UPDATE platillo SET nombre_platillo = ?, descripcion = ?, precio = ?, id_categoria = ? WHERE id_platillo = ?",
+            [platillo.nombre_platillo, platillo.descripcion_platillo, platillo.precio_platillo, platillo.id_categoria, id]
+        );
+        return result.affectedRows > 0 ? platillo : undefined;
+    }
+
+    async eliminarPlatillo(id: number): Promise<boolean> {
+        const [result] = await connection.query<ResultSetHeader>(
+            "DELETE FROM platillo WHERE id_platillo = ?", [id]
+        );
+        return result.affectedRows > 0;
+    }
+}
