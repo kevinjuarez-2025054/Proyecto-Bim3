1106622b1ca6f16ef89d41aeeacb0fcdf3f6fbbe
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-07-24 20:55:16 -0600

    refactor: se cambio la configuracion de la db

--- RestNova/src/config/db.ts
+++ RestNova/src/config/db.ts
@@ -1,19 +1,21 @@
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: "localhost",
     user: "IN5CM",
     password: "?donmoA5m@",
-    database: "DBGestion_Restaurante_in5cm"
+    database: "DBGestion_Restaurante_in5cm",
+    waitForConnections: true,
+    connectionLimit: 10
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error al conectar a la base de datos:', err);
-        return;
-    }else {
-        console.log('Conectado a la base de datos.');
-    }
-});
+connection.getConnection()
+    .then((conn) => {
+        console.log("Conexión a la base de datos establecida");
+        conn.release(); 
+    })
+    .catch((err) => {
+        console.error("Error al conectar a la base de datos:", err);
+    });
 
 export default connection;
